4672c0c9f7f6ed55b8a090c31a6ca0d03457b185
zhtut <zhtg@ubuntu_builder.com>
2026-02-21 00:40:49 +0800

    auto sync packages

--- luci-app-easytier/version.mk
+++
@@ -1,5 +0,0 @@
-# EasyTier Version Configuration
-# This file is the single source of truth for the EasyTier version
-# Used by: easytier/Makefile, luci-app-easytier/Makefile, init.d/easytier
-
-EASYTIER_VERSION=2.5.0
